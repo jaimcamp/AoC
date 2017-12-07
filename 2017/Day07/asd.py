c41ec46261f2fd99569ccc6e4646b37f9cd91ba3
Jaime A. Campos V <jcampos@posteo.net>
2017-12-07 22:32:43 +0100

    Not finish day07

---
+++ 2017/Day07/asd.py
@@ -0,0 +1,24 @@
+import re, collections
+
+with open("example.txt") as fp:
+    text = fp.read()
+
+weight = {}
+children = {}
+for line in text.strip().splitlines():
+    label, n, *xs = re.findall(r'\w+', line)
+    weight[label] = int(n)
+    children[label] = tuple(xs)
+
+root, = set(weight) - {c for cs in children.values() for c in cs}
+
+def total_weight(label):
+    sub = [total_weight(c) for c in children[label]]
+    if len(set(sub)) > 1:
+        (target, _), (failure, _) = collections.Counter(sub).most_common()
+        print(target - failure + weight[children[label][sub.index(failure)]])
+        return weight[label] + sum(sub)
+    return weight[label] + sum(sub)
+
+print(total_weight(root))
+
