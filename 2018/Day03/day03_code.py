f4a7e9606f5fc4f2902038f6df5cfa9d13bde3d4
Jaime A. Campos V <jcampos@posteo.net>
2018-12-04 21:27:11 +0100

    Finish day 3

--- 2018/Day03/day03_code.py
+++ 2018/Day03/day03_code.py
@@ -10,20 +10,49 @@ def translate_coords(line):
     l_split = line.split(" ")
     l_id = l_split[0][1:]
     l_xy = l_split[2][:-1].split(",")
-    l_x, l_y = l_xy
+    l_y, l_x = l_xy
     l_wh = l_split[3].split("x")
-    l_w, l_h = l_wh
-    # l_out = [l_id, l_x, l_y, l_w, l_h]
+    l_h, l_w = l_wh
     l_out = {'id': l_id,
              'x': l_x,
              'y': l_y,
              'width': l_w,
              'height': l_h
              }
+    l_out = {k: int(v) for k, v in l_out.items()}
     return l_out
 
 
-cloth = np.zeros((1000, 1000))
+def create_canvas(n=1000):
+    canvas = np.zeros((n, n), 'int')
+    return canvas
+
+
+def add_to_canvas(canvas, in_dic):
+    canvas[in_dic['x']:in_dic['x'] + in_dic['width'],
+           in_dic['y']:in_dic['y'] + in_dic['height']] += 1
+
+
+def check_canvas(canvas, in_dic):
+    is_one = np.all(
+        canvas[in_dic['x']:in_dic['x'] + in_dic['width'],
+               in_dic['y']:in_dic['y'] + in_dic['height']] == 1)
+    if is_one:
+        print(in_dic['id'])
+
+
 with open("day03_input.txt", "r") as f:
+# with open("example.txt", "r") as f:
+    canvas = create_canvas(1000)
     for line in f:
-        print(translate_coords(line.strip()))
+        in_dic = translate_coords(line.strip())
+        add_to_canvas(canvas, in_dic)
+print(canvas)
+print(np.sum(canvas > 1))
+
+
+with open("day03_input.txt", "r") as f:
+# with open("example.txt", "r") as f:
+    for line in f:
+        in_dic = translate_coords(line.strip())
+        check_canvas(canvas, in_dic)
