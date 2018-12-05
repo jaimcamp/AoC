f5270c8dcffe3ea601a9f24514a9a43ffc4a1703
Campos, Jaime <Jaime.Campos@spectrumk.de>
2018-12-05 17:32:29 +0100

    start day04

---
+++ 2018/Day04/day04_code.py
@@ -0,0 +1,24 @@
+#!/bin/env python3
+import datetime
+
+def to_timestamp(date, time):
+    ts = datetime.datetime.strptime(date + " " + time, "[%Y-%m-%d %H:%M]")
+    return(ts)
+
+
+logs = []
+with open("day04_input.txt", "r") as f:
+    for line in f:
+        date, time, *log = line.strip().split(" ")
+        timestamp = to_timestamp(date, time)
+        tmp_dic = {'timestamp': timestamp, 'log': log}
+        logs.append(tmp_dic)
+logs = sorted(logs, key=lambda x: x['timestamp'])
+for log in logs:
+    if 'Guard' in log['log']:
+        log['status'] = 'start'
+    elif 'falls' in log['log']:
+        log['status'] = 'sleep'
+    elif 'wakes' in log['log']:
+        log['status'] = 'up'
+print(logs)
