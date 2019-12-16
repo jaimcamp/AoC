ce87bc05ea1afc20fe370376c59af3490abbefc1
Jaime A. Campos V <jcampos@posteo.net>
2019-12-16 13:19:52 +0100

    Day16P1

--- 2019/Day16/p2.py
+++ 2019/Day16/p2.py
@@ -15,17 +15,25 @@ def lastd(intt):
     return int(str(intt)[-1])
 
 
+def repandix(string, times=10000):
+    ix = int(string[:7])
+    rep = string * times
+    return rep, ix
+
+
 if __name__ == "__main__":
     rin = read_s('input.txt')
-    ex1 = "12345678"
+    ex1 = "03036732577212944063491565474664"
     ex2 = "80871224585914546619083218645595"
     ex3 = "19617804207202209144916044189917"
     base = [int(x) for x in "0, 1, 0, -1".split(',')]
-    inputt = toint(rin)
+    rep, ix = repandix(ex1)
+    inputt = toint(rep)
     tmp_input = inputt
     length = len(inputt)
     outputt = [0] * length
     for m in range(100):
+        print("m", m)
         for i in range(length):
             base_it = cycle([x for x in base for k in range(i+1)])
             next(base_it)
@@ -38,4 +46,4 @@ if __name__ == "__main__":
         tmp_input = outputt
         outputt = [0] * length
         print(tmp_input)
-    print(tmp_input[:8])
+    print(tmp_input[ix:ix + 8])
