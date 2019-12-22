17364f1c72a50756ec338a2833041d134dc5ee6c
Jaime A. Campos V <jcampos@posteo.net>
2019-12-22 09:55:56 +0100

    Black fixer

--- 2019/Day21/p2.py
+++ 2019/Day21/p2.py
@@ -3,22 +3,23 @@
 from Intcode import Intcode
 
 
-A = Intcode('input.txt')
+A = Intcode("input.txt")
 
 while not A.stop:
     A.run([], False, halt_output=1)
     if not A.stop:
-        print(chr(A.output[0]), end='')
+        print(chr(A.output[0]), end="")
         A.halt = False
         A.reset_output()
 
-instructions = [ord(x) for x in 'NOT C T\nAND D T\nOR T J\nNOT F T\nOR H T\nAND T J\nNOT A T\nOR T J\nNOT B T\nAND A T\nAND D T\nOR T J\nRUN\n']
+instructions = [
+    ord(x)
+    for x in "NOT C T\nAND D T\nOR T J\nNOT F T\nOR H T\nAND T J\nNOT A T\nOR T J\nNOT B T\nAND A T\nAND D T\nOR T J\nRUN\n"
+]
 A.run(instructions)
 for l in A.output:
     try:
-        print(chr(l), end='')
+        print(chr(l), end="")
     except:
         print(l)
 # print(''.join([chr(x) for x in A.output]))
-
-
