ba19574c6ba22ad115a2a4de54fba952b14d88ad
Jaime A. Campos V <jcampos@posteo.net>
2019-12-25 17:39:37 +0100

    Day20

--- 2019/Day20/p2.py
+++ 2019/Day20/p2.py
@@ -14,41 +14,13 @@ def find_neighs(pos, what, dic):
     return out
 
 
-example1 = """
-         A           
-         A           
-  #######.#########  
-  #######.........#  
-  #######.#######.#  
-  #######.#######.#  
-  #######.#######.#  
-  #####  B    ###.#  
-BC...##  C    ###.#  
-  ##.##       ###.#  
-  ##...DE  F  ###.#  
-  #####    G  ###.#  
-  #########.#####.#  
-DE..#######...###.#  
-  #.#########.###.#  
-FG..#########.....#  
-  ###########.#####  
-             Z       
-             Z       
-"""
-
 tiles = defaultdict(lambda: ' ')
-# for ix, line in enumerate(example1.split('\n')):
 for ix, line in enumerate(open('input.txt').read().rstrip('\n').split('\n')):
     for iy, val in enumerate(line):
         if val == "." or val.isalpha():
             tiles[(ix, iy)] = val
-
-G = nx.Graph()
-for key, value in tiles.items():
-    if value == ".":
-        neighs = find_neighs(key, ["."], tiles)
-        [G.add_edge(key, x) for x in neighs]
-# print(G.nodes)
+maxi = max([i for i, j in tiles.keys()])
+maxj = max([j for i, j in tiles.keys()])
 
 pairs = defaultdict(list)
 letters = [(key, value) for key, value in tiles.items() if value.isalpha()]
@@ -63,12 +35,30 @@ for key, value in letters:
         pairs[(tiles[key], tiles[pair])].append(exitpos[0])
     else:
         pairs[(tiles[pair], tiles[key])].append(exitpos[0])
-start = pairs[('A', 'A')][0]
-end = pairs[('Z', 'Z')][0]
+# print(pairs)
+start = (0, *pairs[('A', 'A')][0])
+end = (0, *pairs[('Z', 'Z')][0])
+# print(start, end)
 del pairs[('A', 'A')]
 del pairs[('Z', 'Z')]
-for key, value in pairs.items():
-    G.add_edge(*value)
+print(pairs)
+G = nx.Graph()
+for k in range(100):
+    for key, value in tiles.items():
+        if value == ".":
+            neighs = find_neighs(key, ["."], tiles)
+            key = (k, *key)
+            neighs = [(k, *x) for x in neighs]
+            [G.add_edge(key, x) for x in neighs]
+    for key, values in pairs.items():
+        if (values[0][0] in [2, maxi-2]) or \
+                (values[0][1] in [2, maxj-2]):
+            toadd = [(k, *values[1]), (k+1, *values[0])]
+        elif (values[1][0] in [2, maxi-2]) or \
+                (values[1][1] in [2, maxj-2]):
+            toadd = [(k, *values[0]), (k+1, *values[1])]
+        G.add_edge(*toadd)
+# print(G.edges)
 
 
 print(nx.shortest_path_length(G, start, end))
