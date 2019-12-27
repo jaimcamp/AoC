19f9aa4d9032f51090855610ceb84ebcac35918a
Jaime A. Campos V <jcampos@posteo.net>
2019-12-27 15:32:27 +0100

    Day17

--- 2019/Day17/p2.py
+++ 2019/Day17/p2.py
@@ -32,6 +32,42 @@ def get_inter(tiles):
     return out
 
 
+def nobehind(pos, dire, dic):
+    a, b = pos
+    if dire[0]:
+        neighbours = [(a, b-1), (a, b+1)]
+    if dire[1]:
+        neighbours = [(a-1, b), (a+1, b)]
+    return neighbours
+
+
+def canforward(pos, dire, dic):
+    n_pos = pos = (pos[0] + dire[0], pos[1] + dire[1])
+    if dic.get(n_pos) == "#":
+        return True
+    else:
+        return False
+
+
+def get_change(pos, dire, dic):
+    change = {
+            (0, 1): [(-1, 0), (1, 0)],
+            (0, -1): [(1, 0), (-1, 0)],
+            (1, 0): [(0, 1), (0, -1)],
+            (-1, 0): [(0, -1), (0, 1)]
+            }
+    c, d = change[dire][0]
+    e, f = change[dire][1]
+    a, b = pos
+    if dic.get((a + c, b + d)) == "#":
+        return ((c, d), "R")
+    elif dic.get((a + e, b + f)) == "#":
+        return ((e, f), "L")
+    else:
+        print("ERROR", pos, dire)
+        return (pos, 99)
+
+
 if __name__ == "__main__":
     A = Intcode("input.txt")
     A.run([], False)
@@ -45,29 +81,51 @@ if __name__ == "__main__":
             continue
         tiles[(i, j)] = chr(code)
         i += 1
-    printit(tiles)
+    # tiles[(10, 55)] = "O"
+    # printit(tiles)
     intersections = get_inter(tiles)
     print("Part 1 ", sum([x * y for x, y in intersections]))
 
     # Part 2
 
     start = [key for key, value in tiles.items() if value == "^"][0]
+    start_dire = (0, -1)
+    pos, dire = start, start_dire
     print(start)
-    # A = Incode("input.txt")
-    # A.input[0] = 2
-    # program = "A,C,B\nR,6,L,10\n10,R,6\nR,8,R,8,R,12,L,8,L\nn\n"
-    # run_codes = [ord(c) for c in program][::-1]
-    # print(run_codes)
-    # A.run(run_codes, False)
-    # codes = A.output
-    # positions = defaultdict(lambda: 32)
-    # i, j = 0, 0
-    # # out = codes.pop()
-    # for code in codes:
-        # if code == 10:
-            # i = 0
-            # j += 1
-            # continue
-        # positions[(i, j)] = code
-        # i += 1
-    # printit(positions)
+    nomore = False
+    steps = 0
+    path = []
+    while not nomore:
+        if canforward(pos, dire, tiles):
+            pos = (pos[0] + dire[0], pos[1] + dire[1])
+            steps += 1
+        else:
+            # print(pos, dire)
+            path.append(str(steps))
+            steps = 0
+            dire, val = get_change(pos, dire, tiles)
+            if val == 99:
+                nomore = True
+                continue
+            path.append(str(val))
+    print("".join(path[1:]))
+
+    A = Intcode("input.txt")
+    A.input[0] = 2
+    program = "A,B,A,C,B,C,A,B,A,C\nR,6,L,10,R,8,R,8\nR,12,L,8,L,10\nR,12,L,10,R,6,L,10\nn\n"
+    run_codes = [ord(c) for c in program]
+    print(run_codes)
+    A.run(run_codes, False)
+    codes = A.output
+    positions = defaultdict(lambda: " ")
+    i, j = 0, 0
+    # out = codes.pop()
+    for code in codes[:-1]:
+        if code == 10:
+            i = 0
+            j += 1
+            continue
+        positions[(i, j)] = chr(code)
+        i += 1
+    printit(positions)
+    print(codes[-1])
